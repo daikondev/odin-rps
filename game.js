e7b577b2e72535b6e890fd645a75cd75e0eb3950
whiteraddish <whiteraddishdev@gmail.com>
2023-02-08 16:20:05 +0200

    Prettied up formatting.

--- game.js
+++ game.js
@@ -1,12 +1,8 @@
-//Rock, Paper, Scissors
-//Simplest way I could think is to select a random index
-//Of the choice array.
 const choice = ['Rock','Paper','Scissors'];
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random() * choice.length);
     return choice[computerChoice]
 }
-
 function playRound(playerSelection, computerSelection){
     const options = {
         Rock: {weakTo: 'Paper', strongTo: 'Scissors'},
@@ -19,7 +15,6 @@ function playRound(playerSelection, computerSelection){
         return "Computer wins!";
     }
 }
-
 function game(){
     let playerPoints = 0;
     let computerPoints = 0;
